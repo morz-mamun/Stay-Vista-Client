502d0807a572ae65df3b23aa1b5269f159af2ae7
unknown <morzmamun@gmail.com>
2024-01-07 22:09:43 +0600

    category dynamic route created using by query string

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,15 +1,22 @@
+import { useSearchParams } from "react-router-dom";
 import CategoryBox from "./CategoryBox";
 import { categories } from "./CategoryData/CategoryData";
 
-
 const Categories = () => {
-    return (
-        <div className="mb-10 flex justify-between overflow-x-auto">
-           {
-            categories.map(category => <CategoryBox key={category.label} label={category.label} icon={category.icon}></CategoryBox>)
-           } 
-        </div>
-    );
+    const [params, setParams] = useSearchParams()
+    const activeCategory = params.get('category')
+  return (
+    <div className="mb-10 flex justify-between overflow-x-auto">
+      {categories.map((category) => (
+        <CategoryBox
+          key={category.label}
+          label={category.label}
+          icon={category.icon}
+          selected= {activeCategory === category.label}
+        ></CategoryBox>
+      ))}
+    </div>
+  );
 };
 
 export default Categories;
--- src/components/Categories/CategoryBox.jsx
+++ src/components/Categories/CategoryBox.jsx
@@ -1,11 +1,36 @@
+import queryString from "query-string";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
-const CategoryBox = ({label, icon: Icon}) => {
-    return (
-        <div className="flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:font-bold transition cursor-pointer hover:border-red-600">
-            <Icon size={26}></Icon>
-            <div>{label}</div>
-        </div>
-    );
+const CategoryBox = ({ label, icon: Icon, selected }) => {
+  const [params, setParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = queryString.parse(params.toString());
+
+      const updatedQuery = { ...currentQuery, category: label };
+
+      const url = queryString.stringifyUrl({
+        url: "/",
+        query: updatedQuery,
+      });
+
+      navigate(url);
+    }
+  };
+  params.get("category");
+  return (
+    <div
+      onClick={handleClick}
+      className={`flex flex-col items-center justify-center gap-2 p-3 hover:font-bold transition cursor-pointer hover:border-red-600 ${selected ? 'border-b-2 border-red-600' : ''}`}
+    >
+      <Icon size={26}></Icon>
+      <div>{label}</div>
+    </div>
+  );
 };
 
 export default CategoryBox;
